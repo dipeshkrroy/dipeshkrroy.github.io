8221fe7dae5fa08216fb4d9f3061aa45518daa18
dipeshkrroy <dipeshkrroy@gmail.com>
2018-01-26 01:51:40 +0530

    update

--- index.js
+++ index.js
@@ -101,13 +101,14 @@ $("#google-signin").click(
 function(){	
 	var provider = new firebase.auth.GoogleAuthProvider();
 	//provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
+ffirebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // ...
+  }
   // The signed-in user info.
   var user = result.user;
-  //alert("hi"+user.email);
-  // ...
 }).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
